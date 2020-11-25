e6dbc57c954d59a3e053e088f04156d258a43bc8
Kilo Meow <dest@disr.it>
2020-11-25 18:47:29 +0300

    update assets paths

--- js/sound.js
+++ js/sound.js
@@ -1,9 +1,9 @@
-var bgmusic = new Audio('winter_tales.mp3');
+var bgmusic = new Audio('assets/music/winter_tales.mp3');
 
 function startBgMusic() {
     bgmusic.play();
 }
 
-var typingSound = new Audio('typing.ogg');
-typingSound.volume = 0.3;
+var typingSound = new Audio('assets/sound/typing.ogg');
+typingSound.volume = 0.5;
 
