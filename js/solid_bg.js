d127e84ef76e2cce9f236f2df928ab9d52faa35a
Kilo Meow <dest@disr.it>
2020-12-08 03:14:44 +0300

    add flashback and updated bg behaviour for animation

--- js/solid_bg.js
+++ js/solid_bg.js
@@ -2,14 +2,33 @@ var c = document.createElement('canvas');
 
 const drag = 30;
 
-var img = new Image();
+var frameArray = [];
+var totalFrames = 0;
+var currentFrameIndex = -1;
+var currentFrame = null;
 
 function preloadImages() {
-    img.src = document.querySelector('components img').src;
+    bg = document.querySelector('components #background');
+    frameInterval = bg.getAttribute("interval") || 1000;
+    document.querySelectorAll('components #background img').forEach(
+        img => {
+            new_img = new Image();
+            new_img.src = img.src;
+            console.log("preload", img.src);
+            frameArray.push(new_img);
+            totalFrames += 1;
+            }
+    );
+    if (totalFrames) {nextFrame()}
+}
+
+function nextFrame() {
+    currentFrameIndex = (currentFrameIndex + 1) % totalFrames;
+    currentFrame = frameArray[currentFrameIndex];
 }
 
 function draw() {
-    var scale = Math.max((height+2*drag)/img.height, (width+2*drag)/img.width);
+    var scale = Math.max((height+2*drag)/currentFrame.height, (width+2*drag)/currentFrame.width);
 
     var ctx = c.getContext('2d');
 
@@ -21,14 +40,16 @@ function draw() {
     ctx.fillStyle = "#152424";
     ctx.fill();
 
-    ctx.drawImage(img, -drag*moveX - drag, -drag*moveY - drag, img.width*scale, height+2*drag);
+    ctx.drawImage(currentFrame, -drag*moveX - drag, -drag*moveY - drag, currentFrame.width*scale, height+2*drag);
 }
 
 function setBg() {
+    preloadImages();
+
     c.classList.add("scene");
     
     updateBG();
-    
+    if (totalFrames > 1) {setInterval(nextFrame, frameInterval);}    
     document.body.style.width = window.innerWidth;
     document.body.style.height = window.innerHeight;
 
