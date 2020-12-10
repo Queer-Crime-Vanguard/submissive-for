80021cfa81ef78d7ac3ffabe53cecf37585e1293
Nataliya Tyshkevich <natasha@api.ai>
2020-12-10 03:29:03 +0300

    fix reflection box hide

--- js/reflection.js
+++ js/reflection.js
@@ -3,5 +3,7 @@ function showReflection() {
     if (current) {current.classList.remove("visible")
                   current.classList.add("shown")}
     newref = document.querySelector(".reflection:not(.shown)")
-    if (newref) {newref.classList.add("visible")}
+    if (newref)
+        {newref.classList.add("visible")}
+    else {document.querySelector("#reflection-box").style.display = "none";}
 }
