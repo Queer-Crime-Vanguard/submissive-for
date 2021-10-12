323576aa5285d89d9b418261bf7420fe4ed1446a
Kilo Meow <dest@disr.it>
2021-10-12 14:19:45 +0300

    music update

--- build/js/music.js
+++ build/js/music.js
@@ -1,29 +1,45 @@
-function bg_audio(name) {
-    let a = new Audio('assets/music/'+name+'.mp3')
+function vibe_audio(name) {
+    let a = new Audio('assets/music/vibes/'+name+'.mp3')
     a.volume = 0;
     a.loop = true;
     return a
 }
 
+let current_playing = new Array();
+
 let mus_vibes = ['basic', 'nervous', 'insight', 'calm']
 
-let bgmusic = {
-    basic: bg_audio('basic'),
-    nervous: bg_audio('nervous'),
-    insight: bg_audio('insight'),
-    calm: bg_audio('calm')
+let vibes_audio = {
+    basic: vibe_audio('basic'),
+    nervous: vibe_audio('nervous'),
+    insight: vibe_audio('insight'),
+    calm: vibe_audio('calm')
 }
 
 let current_vibe = 'basic';
 
 let started = false;
 
-function startBgMusic() {
+let music_audio = {}
+
+function playMusic(name) {
+    let a = music_audio[name]
+    if (a == null) {
+        a = new Audio('assets/music/'+name+'.mp3')
+        music_audio[name] = a
+    }
+    current_playing.push(a)
+    a.play()
+}
+
+function startVibe() {
     if (!started) {
         mus_vibes.forEach(
             (vibe) => {
-                bgmusic[vibe].play()
-                if (vibe == current_vibe) {bgmusic[vibe].volume = 1}
+                let a = vibes_audio[vibe]
+                a.play()
+                if (vibe == current_vibe) {a.volume = 1}
+                current_playing.push(a)
             }
         )
         started = true;
@@ -41,19 +57,25 @@ function switchVibe(new_vibe) {
     let crossfade = true;
     setInterval(() => {
         if (crossfade) {
-            bgmusic[current_vibe].volume = v_d(bgmusic[current_vibe].volume, -fadeStep)
-            bgmusic[new_vibe].volume = v_d(bgmusic[new_vibe].volume, fadeStep)
+            vibes_audio[current_vibe].volume = v_d(vibes_audio[current_vibe].volume, -fadeStep)
+            vibes_audio[new_vibe].volume = v_d(vibes_audio[new_vibe].volume, fadeStep)
         }
     }, crossFadeLength*fadeStep)
     setTimeout(() => {
         crossfade = false;
-        bgmusic[current_vibe].volume = 0;
-        bgmusic[new_vibe].volume = 1;
+        vibes_audio[current_vibe].volume = 0;
+        vibes_audio[new_vibe].volume = 1;
         current_vibe = new_vibe
     }, crossFadeLength)
 }
 
-document.addEventListener("play_bg_music", startBgMusic);
+function stopPlaying() {
+    current_playing.forEach((a) => {a.pause()})
+    current_playing = new Array()
+}
+
+document.addEventListener("play_vibe", startVibe)
 document.addEventListener("switch_vibe", (d) => {
     console.log('update vibe', d.detail.vibe)
     switchVibe(d.detail.vibe)})
+document.addEventListener("stop_playing", stopPlaying)
